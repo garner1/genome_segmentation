f9466beb1f8774bc5f7777995dc4b48c7a988911
garner1 <silvano.garnerone@gmail.com>
2017-06-12 09:03:39 +0200

    added R script for single chromosome analysis

---
+++ get_kmer_oneChrOnly.R
@@ -0,0 +1,65 @@
+library(Biostrings)
+require(BSgenome.Hsapiens.UCSC.hg19)
+library(data.table)  
+
+
+# chromosome names
+cnames = seqnames(Hsapiens)[1:24]
+
+# initialization (out_dim can always be set to 1 wo loss of generalization)
+in_dim = 3
+out_dim = 1
+
+name = cnames[21]
+print(name)
+seqChr = unmasked(Hsapiens[[name]]) # get the chromosome sequence
+  
+# Move from left to right
+Tmat_LR = oligonucleotideTransitions(seqChr, left = in_dim, right = out_dim, as.prob = FALSE)
+# generate windows on the chromosome:
+windows <- Views(seqChr, start=seq(from=1,to=length(seqChr)-(in_dim + out_dim), by=1), width = in_dim+out_dim)
+# transform Views to a dataframe to be able to write to file using an efficient library
+df <- rbind(data.frame(start=start(windows), end=end(windows), seq=as.character(windows)))
+filename <- paste('~/Work/dataset/genome_segmentation/views_',as.character(name),"_",as.character(in_dim),"to",as.character(out_dim),'_LR.csv', sep='')
+fwrite(x=df, file=filename, append=FALSE, row.names=FALSE)
+  
+# Move from right to left
+
+Tmat_RL = oligonucleotideTransitions(reverse(seqChr), left = in_dim, right = out_dim, as.prob = FALSE)
+# generate windows on the chromosome:
+windows <- Views(reverse(seqChr), start=seq(from=1,to=length(seqChr)-(in_dim + out_dim), by=1), width = in_dim+out_dim)
+# transform Views to a dataframe to be able to write to file using an efficient library
+df <- rbind(data.frame(start=start(windows), end=end(windows), seq=as.character(windows)))
+filename <- paste('~/Work/dataset/genome_segmentation/views_',as.character(name),"_",as.character(in_dim),"to",as.character(out_dim),'_RL.csv', sep='')
+fwrite(x=df, file=filename, append=FALSE, row.names=FALSE)
+Tmat_LR_norm <- Tmat_LR/rowSums(Tmat_LR)
+Tmat_RL_norm <- Tmat_RL/rowSums(Tmat_RL)
+
+key <- c()
+value <- c()
+i <- 0
+for(row in 1:4**in_dim){
+  for(col in 1:4**out_dim){
+    i <- i+1
+    key[[i]] <- paste(rownames(Tmat_LR)[row],colnames(Tmat_LR)[col],sep = '')
+    value[[i]] <- Tmat_LR_norm[row,col]
+  }
+}
+df <- data.frame(key,value)
+filename <- paste("~/Work/dataset/genome_segmentation/transitionMatrix_",as.character(in_dim),"to",as.character(out_dim),"_LR.csv", sep = '')
+fwrite(x=df, file=filename, append=FALSE, row.names=TRUE)
+# write.csv(df, file = filename)
+
+key <- c()
+value <- c()
+i <- 0
+for(row in 1:4**in_dim){
+  for(col in 1:4**out_dim){
+    i <- i+1
+    key[[i]] <- paste(rownames(Tmat_RL)[row],colnames(Tmat_RL)[col],sep = '')
+    value[[i]] <- Tmat_RL_norm[row,col]
+  }
+}
+df <- data.frame(key,value)
+filename <- paste("~/Work/dataset/genome_segmentation/transitionMatrix_",as.character(in_dim),"to",as.character(out_dim),"_RL.csv", sep = '')
+fwrite(x=df, file=filename, append=FALSE, row.names=TRUE)
