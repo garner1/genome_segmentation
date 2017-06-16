d8f37177bebc7ef36aa1ff5442b6676ec0792c67
garner1 <silvano.garnerone@gmail.com>
2017-06-16 17:25:42 +0200

    the code for generating the co-occurence matrix has been generated

---
+++ count_pairs.py
@@ -0,0 +1,32 @@
+#! /usr/bin/python
+
+from itertools import combinations
+from collections import Counter
+import os.path
+import sys
+import re
+
+file_name = str(sys.argv[1])    # ex: $datadir/word_{forward,reverse}.dic
+window = int(sys.argv[2])
+
+def collect_pairs(file_name,window):
+    pair_counter = Counter()
+    with open(file_name, 'rb') as infile:
+        count = 0
+        context = []
+        for line in infile:
+            count += 1
+            lista = re.split('\t',line.strip())
+            context.append(lista[3])
+            if count > window:
+                for index in range(count-window+1,count):
+                    pair_counter[(context[count-window],context[index])] += 1
+    return pair_counter
+    
+p = collect_pairs(file_name,window)
+
+with open(file_name + "_counter.txt",'w') as f:
+    for k,v in  p.most_common():
+        f.write( "{} {}\n".format(k,v) )
+
+print 'Done with' + file_name
